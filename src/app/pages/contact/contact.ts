0be2ab368fe5c1f6e0a198637748e635c2ae0582
shahinda <shahindashawki.00@gmail.com>
2025-09-27 22:44:04 +0300

    add contact

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   standalone: false,
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './contact.scss'
 })
 export class Contact {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: [''],
+      subject: [''],
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      alert('✅ تم إرسال رسالتك بنجاح');
+      this.contactForm.reset();
+    }
+  }
 }
